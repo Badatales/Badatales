9d205d9a751cb39ee0f7ee42c5a28abae41ed8d2
Badatales <talesofbada@gmail.com>
2026-02-15 00:28:45 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,16 +70,16 @@
 		&&	$header.hasClass('alt')) {
 
 			$window.on('resize', function() { $window.trigger('scroll'); });
-/*
+
 			$banner.scrollex({
 				bottom:		$header.outerHeight() + 1,
 				terminate:	function() { $header.removeClass('alt'); },
 				enter:		function() { $header.addClass('alt'); },
 				leave:		function() { $header.removeClass('alt'); }
 			});
-*/
 		}
 
 
 })(jQuery);
+
 
